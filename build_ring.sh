e72f09b57dc178dbcdb1b73e67ab51e99c242a17
songkuangshi <kuangshi@kuangshi.info>
2018-12-20 14:06:30 +0800

    update travis

---
+++ build_ring.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+if [ $# -lt 2 ]; then
+    echo "usage: $0 [worker_num] [master_ip_port tcp://127.0.0.1:17832]"
+    exit -1;
+fi
+
+export LightCTR_WORKER_NUM=$1
+shift
+export LightCTR_MASTER_ADDR=$1
+
+make ring_master &
+make ring_worker &
+
+wait
+echo
+echo
+echo "[Build Success]"
+echo "Please copy different BIN file to corresponding machine, run Master first"
+echo
+echo "[or] Press any key to run clunster on standalone mode"
+read -n 1
+
+./LightCTR_BIN_Ring_Master &
+
+for ((i=0; i<${LightCTR_WORKER_NUM}; ++i)); do
+    ./LightCTR_BIN_Ring_Worker &
+done
+
+wait
